13aea0ffa633ca24fe0f0b1e4897466040854eaa
Aisenidze <117153685+Aisenidze@users.noreply.github.com>
2023-05-11 00:05:31 +0900

    Revert "Sprint 11"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,45 +1,17 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import AppHeader from '../AppHeader/AppHeader';
-import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
-import Modal from '../Modal/Modal';
+import styles from "./app.module.css";
+import { data } from "../../utils/data";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch("https://norma.nomoreparties.space/api/ingredients")
-      .then(res => res.json())
-      .then((result) => {
-        setIsLoaded(true)
-        setItems(result.data)
-      }, 
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      })
-  },[])
-console.log(items);
-  if (error) {
-    return <div>Ошибка: {error}</div>;
-  } else if (!isLoaded) {
-    return <div>Загрузка...</div>;
-  } else {
-    return (
-      <div className='main'>
-        <AppHeader/>
-        <div className='template'>
-          <div className='wrapper'>
-            <BurgerIngredients items={items}/>
-            <BurgerConstructor items={items}/>
-          </div>
-        </div>
-        {/* <button onClick={() => setOpen(true)}>Открыть/Закрыть</button> */}
-      </div>
-    );
-  }
+  return (
+    <div className={styles.app}>
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	Измените src/components/app/app.jsx и сохраните для обновления.
+      </pre>
+    </div>
+  );
 }
+
 export default App;
