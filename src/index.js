4af31ce273696050be1af1e29967e778436c22d3
Aisen Ivanov <Aisenidze@gmail.com>
2023-06-08 22:21:58 +0900

    changed name app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import App from "./components/App/App";
+import App from "./components/App/1234";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
